c37579d92f2bbf2c8e64d3b2e858bcc57d186e85
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-08-13 20:42:37 +0530

    feat: created src

---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import { makeStyles } from "@mui/styles";
+
+
+const useStyles = makeStyles({
+    logo: {
+        width: 100,
+        padding: 5
+    },
+    navbar: {
+        background: ['#000', '!important'],
+        position: ['static', '!important'],
+        height: [50, '!important']
+    }
+})
+
+const Header = () => {
+    const classes = useStyles();
+    const logo = 'https://miro.medium.com/max/802/1*dLWPk_rziSpWhPx1UWONbQ@2x.png';
+
+    return (
+        <>
+            <img src={logo} alt="logo" className={classes.logo} />    
+            {/* <AppBar className={classes.navbar}>
+                <Toolbar>
+                </Toolbar>
+            </AppBar> */}
+        </>
+    )
+}
+
+export default Header;
